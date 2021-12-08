71355b3b5f1f81a194942c7eadcad94b55d7947e
lzx <1925488382@qq.com>
2021-12-08 17:03:57 +0800

    hot-fix 第一次提交

--- src/main/java/com/test/demo/test.java
+++ src/main/java/com/test/demo/test.java
@@ -15,6 +15,8 @@ public class test {
 
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
 //        Book book = new Book();
 
         return "test";
